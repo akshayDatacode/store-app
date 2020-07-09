31a9a124a40c10cae25a578a407c41e17bfd6d30
akshayDatacode <akshaywebllisto@gmsil.com>
2020-07-09 22:17:33 +0530

    Create CAT and Showing data into Cart

--- src/components/CartComponent.jsx
+++ src/components/CartComponent.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
-class Cart extends Component {
+import { connect } from "react-redux";
+
+class CartComponent extends Component {
   state = {};
   render() {
     return (
       <>
         <h1>Shopping Cart</h1>
+        <div className="row">
+          <div className="col">
+            HEllo
+            {this.props.cart.map((item) => (
+              <div className="card mb-3 shadow-lg border">
+                HELLO
+                <div className="card-body">
+                  <h3 className="text-primary">{item.title}</h3>
+                  <h6>Price : {item.price} /-</h6>
+                  <p>Size : {item.size}</p>
+                  <b className="text-success">Discount : {item.discount} %</b>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </>
     );
   }
 }
 
-export default Cart;
+const mapStateToProps = (state) => {
+  return {
+    cart: state.cart,
+  };
+};
+
+export default connect(mapStateToProps)(CartComponent);
