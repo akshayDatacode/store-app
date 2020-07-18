a032818419d50b3913c341df2d39aa55bc07eef3
akshayDatacode <akshaywebllisto@gmsil.com>
2020-07-18 18:38:30 +0530

    Header design improvment

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -21,7 +21,7 @@ import {
   resumeUser,
 } from "../redux/product/productAction";
 
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 import SignupModal from "./SignupModal";
 import LoginModal from "./LoginModal";
@@ -62,9 +62,11 @@ class HeaderComponent extends Component {
   render() {
     return (
       <>
-        <nav class="navbar navbar-light bg-secondary">
-          <Link class="navbar-brand" to="/">
-            <h3
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand>
+            <Link
+              class="navbar-brand"
+              to="/"
               className="text-warning"
               onClick={this.props.getProductsFromCart}
             >
@@ -74,86 +76,96 @@ class HeaderComponent extends Component {
                 className="mr-1 text-white"
               />
               Store
-            </h3>
-          </Link>
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            {this.props.user && (
+              <Nav className="mr-5 text-left">
+                <Nav.Link>
+                  <Link to="/add_product">
+                    <div className="btn btn-outline-warning my-2 my-sm-0 ">
+                      Add Product
+                    </div>
+                  </Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Dropdown>
+                    <Dropdown.Toggle variant="warning" id="dropdown-basic">
+                      <FontAwesomeIcon icon={faAlignRight} className="mr-2" />
+                      Filter
+                    </Dropdown.Toggle>
 
-          {this.props.user ? (
-            <>
-              <Link to="/add_product">
-                <div className="btn btn-outline-warning my-2 my-sm-0 ">
-                  Add Product
-                </div>
-              </Link>
-              <Dropdown>
-                <Dropdown.Toggle variant="warning" id="dropdown-basic">
-                  <FontAwesomeIcon icon={faAlignRight} className="mr-2" />
-                  Filter
-                </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item onClick={this.props.priceLowToHighFilter}>
+                        Price Low
+                      </Dropdown.Item>
+                      <Dropdown.Item onClick={this.props.priceHighToLowFilter}>
+                        Price High
+                      </Dropdown.Item>
+                      <Dropdown.Item onClick={this.props.ascendingFilter}>
+                        A - Z
+                      </Dropdown.Item>
+                      <Dropdown.Item onClick={this.props.descendingFilter}>
+                        Z - A
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Nav.Link>
+              </Nav>
+            )}
+            {/* Right Side Section */}
+            <Nav className="ml-auto text-right">
+              {this.props.user ? (
+                <>
+                  <Nav.Link>
+                    <Link to="/cart" onClick={this.props.getProductsFromCart}>
+                      <div className="btn btn-outline-warning my-2 my-sm-0 pb-0">
+                        <h4 className="text-white">
+                          <FontAwesomeIcon
+                            icon={faCartPlus}
+                            color="orange"
+                            className="mr-2"
+                            size="1x"
+                          />
+                          {this.props.cartCount}
+                        </h4>
+                      </div>
+                    </Link>
+                  </Nav.Link>
 
-                <Dropdown.Menu>
-                  <Dropdown.Item onClick={this.props.priceLowToHighFilter}>
-                    Price Low
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={this.props.priceHighToLowFilter}>
-                    Price High
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={this.props.ascendingFilter}>
-                    A - Z
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={this.props.descendingFilter}>
-                    Z - A
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-
-              <div
-                className="btn btn-outline-light my-2 my-sm-0 pb-0"
-                onClick={this.props.logout}
-              >
-                <h6 className="text-dark">
-                  <FontAwesomeIcon
-                    icon={faDoorClosed}
-                    color="orange"
-                    className="mr-2"
-                    size="1x"
-                  />
-                  LogOut
-                </h6>
-              </div>
-              <Link to="/cart" onClick={this.props.getProductsFromCart}>
-                <div className="btn btn-outline-light my-2 my-sm-0 pb-0">
-                  <h4 className="text-dark">
-                    <FontAwesomeIcon
-                      icon={faCartPlus}
-                      color="orange"
-                      className="mr-2"
-                      size="1x"
-                    />
-                    {this.props.cartCount}
-                  </h4>
-                </div>
-              </Link>
-            </>
-          ) : (
-            <>
-              <div
-                className="btn btn-outline-light my-2 my-sm-0 pb-0"
-                onClick={this.props.handleSignup}
-              >
-                <h4 className="text-dark">Signup</h4>
-              </div>
-              <SignupModal show={this.props.show} />
-
-              <div
-                className="btn btn-outline-light  pb-0"
-                onClick={this.props.handleLogin}
-              >
-                <h4 className="text-dark">login</h4>
-              </div>
-              <LoginModal show={this.props.showlogin} />
-            </>
-          )}
-        </nav>
+                  <Nav.Link>
+                    <div
+                      className="btn my-2 my-sm-0 pb-0"
+                      onClick={this.props.logout}
+                    >
+                      <h6 className="text-light">
+                        <FontAwesomeIcon
+                          icon={faDoorClosed}
+                          color="orange"
+                          className="mr-2"
+                          size="1x"
+                        />
+                        LogOut
+                      </h6>
+                    </div>
+                  </Nav.Link>
+                </>
+              ) : (
+                <Nav.Link>
+                  <div
+                    className="btn my-2 my-sm-0 pb-0"
+                    onClick={this.props.handleSignup}
+                  >
+                    <h6 className="text-light">Signup / Login</h6>
+                  </div>
+                  <SignupModal show={this.props.show} />
+                  <LoginModal show={this.props.showlogin} />
+                </Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </>
     );
   }
